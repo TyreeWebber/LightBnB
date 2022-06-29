ff7d36d470586ea5df2a86beb05bde51798f0260
Tyree Webber <webber.tyree@gmail.com>
2022-06-29 13:37:27 -0600

    add select queries in 1_queries directory

---
+++ 1_queries/3_property_listings_by_city.sql
@@ -0,0 +1,7 @@
+SELECT properties.id, properties.title, properties.cost_per_night, avg(property_reviews.rating) AS average_rating
+FROM properties JOIN property_reviews ON property_id = properties.id
+WHERE properties.city LIKE '%Vancouver%'
+GROUP BY properties.id
+HAVING avg(property_reviews.rating) >= 4
+ORDER BY cost_per_night
+LIMIT 10;
